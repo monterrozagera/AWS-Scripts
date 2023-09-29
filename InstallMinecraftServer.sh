e121a4b6e8205a3b6e4322133320a45d7934341e
Monterroza, Gerardo <91101951+monterrozagera@users.noreply.github.com>
2023-09-29 11:05:59 -0700

    Update InstallMinecraftServer.sh
    Fix Install script

--- InstallMinecraftServer.sh
+++ InstallMinecraftServer.sh
@@ -4,19 +4,19 @@ sudo apt update -y
 sudo apt install openjdk-17-jre-headless -y
 sudo ufw allow 25565
 sudo useradd -r -m -U -d /opt/minecraft -s /bin/bash minecraft
-sudo su - minecraft
-mkdir -p ~/{backups,tools,server}
+# sudo su - minecraft
+mkdir -p /opt/minecraft/{backups,tools,server}
 # cd ~/tools && git clone https://github.com/Tiiffi/mcrcon.git
 # cd ~/tools/mcrcon
 # gcc -std=gnu11 -pedantic -Wall -Wextra -O2 -s -o mcrcon mcrcon.c
-cd ~/server
-wget https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar -O minecraft_server.1.20.1.jar
-sudo java -Xms1024M -Xmx1024M -jar minecraft_server.1.20.1.jar nogui
+cd /opt/minecraft/server
+wget https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar -O minecraft_server.1.20.2.jar
+java -Xms1024M -Xmx1024M -jar minecraft_server.1.20.2.jar nogui
 head -n -1 eula.txt > eula1.txt
 echo "eula=true" >> eula1.txt
 rm eula.txt
 mv eula1.txt eula.txt
-sudo echo "[Unit]
+echo "[Unit]
 
 Description=Minecraft Server
 
@@ -42,10 +42,11 @@ NoNewPrivileges=true
 
 WorkingDirectory=/opt/minecraft/server
 
-ExecStart=/usr/bin/java -Xmx4G -Xms1024M -jar minecraft_server.1.20.1.jar nogui
+ExecStart=/usr/bin/java -Xmx4G -Xms1024M -jar minecraft_server.1.20.2.jar nogui
 
 [Install]
 
 WantedBy=multi-user.target" > /etc/systemd/system/minecraft.service
-sudo systemctl start minecraft
-sudo systemctl enable minecraft
+sudo chown minecraft:minecraft /opt/minecraft/*
+systemctl start minecraft
+systemctl enable minecraft
